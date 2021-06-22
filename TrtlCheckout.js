a75ac3b858b594ed44e85a29787591a6c5271914
Benjamin Collins <benjamin@collins.moe>
2021-06-22 12:05:19 +0000

    turtlepay confusion

--- TrtlCheckout.js
+++ TrtlCheckout.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const fetch = require('node-fetch')
 const express = require('express')
 const app = express()
 const port = 2800
@@ -7,7 +8,7 @@ const port = 2800
 const addr = 'TRTLv3bpWo5KR6ACMJ4cJeXq2CVtEwEwZhTJvSyriKjYjWEMS9Rb9Wwf4mDo3WTzDZNP1gPHYp8bJ7VYCgbHGxcvgFjXPUsUcgB';
 const view = '9fcb0087252c147657af23700810cad0bceee0b4fdf2a4479406b9f2636eae0d';
 
-app.post('/trtl/prepare', function(req, res) {
+app.get('/trtl/prepare', async function(req, res) {
 
 	const body = {
 		amount : 100000,
@@ -16,9 +17,29 @@ app.post('/trtl/prepare', function(req, res) {
 		callback : "https://trtl.dashgl.com/trtl/process"
 	}
 
+	let prep = await fetch('https://api.turtlepay.io/v2/new', {
+		headers: {
+			'Accept' : 'application/json',
+			'Content-Type' : 'application/json'
+    	},
+    	method : "POST",
+    	body: JSON.stringify(body)
+	});
+
+	console.log(prep);
+
+	let data = await prep.json();
+
+	console.log(data);
+
+	res.json(data);
+
 });
 
 app.all('/trtl/process', function(req, res) {
+
+	console.log('ConfIRMED PAYMENT!!!');
+	console.log(req.body);
 
 });
 
